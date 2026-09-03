70d81c7bf450a3fdd6d7f6c3267223aa5fa3ec9f
dog <13100162717@163.com>
2026-09-03 18:13:23 +0800

    完善关于我们页面和设置页链接样式

--- miniprogram/pages/about/about.ts
+++ miniprogram/pages/about/about.ts
@@ -1,1 +1,29 @@
-Page({})
+const githubUrl = 'https://github.com/wtx1717/duaa-power-reminder'
+const email = '13100162717@163.com'
+
+function copyText(data: string, title: string) {
+  wx.setClipboardData({
+    data,
+    success() {
+      wx.showToast({
+        title,
+        icon: 'success',
+      })
+    },
+  })
+}
+
+Page({
+  data: {
+    githubUrl,
+    email,
+  },
+
+  onCopyGithub() {
+    copyText(githubUrl, '已复制链接')
+  },
+
+  onCopyEmail() {
+    copyText(email, '已复制邮箱')
+  },
+})
